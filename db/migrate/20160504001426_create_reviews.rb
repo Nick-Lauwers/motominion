755d3d0e289d129b2e39c0f32732c8fd282df81d
yukon519 <nicholaslauwers04@gmail.com>
2017-01-25 19:50:05 +0000

    Debug reviews migration
    Former-commit-id: a5596956a342e4a8d79fbebfb6635e15aed51672

--- db/migrate/20160504001426_create_reviews.rb
+++ db/migrate/20160504001426_create_reviews.rb
@@ -5,9 +5,9 @@ class CreateReviews < ActiveRecord::Migration
       t.string :title
       t.text :comment
       t.integer :rating
+      t.references :reviewer
+      t.references :reviewed
       t.references :vehicle,  foreign_key: true
-      t.references :reviewer, foreign_key: true
-      t.references :reviewed, foreign_key: true
       
       t.timestamps null: false
     end
@@ -15,5 +15,8 @@ class CreateReviews < ActiveRecord::Migration
     add_index :reviews, [:vehicle_id,  :created_at]
     add_index :reviews, [:reviewer_id, :created_at]
     add_index :reviews, [:reviewed_id, :created_at]
+    
+    add_foreign_key :reviews, :reviewer
+    add_foreign_key :reviews, :reviewed
   end
 end
