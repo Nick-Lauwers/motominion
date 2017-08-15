7f2f25a83e0b05ae8a5e46ff3d0da3196a67888f
yukon519 <nicholaslauwers04@gmail.com>
2017-08-15 01:05:25 +0000

    Correct omniauth callbacks controller
    Former-commit-id: 3d7b15593f3fc5aed441cd04977a3d25f9eba582

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,33 +2,33 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def stripe_connect
     
-    auth_data = request.env["omniauth.auth"]
-    @user     = current_user
+    # auth_data = request.env["omniauth.auth"]
+    # @user     = current_user
     
-    if @user.persisted?
+    # if @user.persisted?
       
-      @user.merchant_id = auth_data.uid
+    #   @user.merchant_id = auth_data.uid
       
-      @user.save
+    #   @user.save
       
-      if @user.merchant_id.blank?
+    #   if @user.merchant_id.blank?
         
-        account = Stripe::Account.retrieve(current_user.merchant_id)
-        account.payout_schedule.delays_days = 7
-        account.payout_schedule.interval    = 'daily'
+    #     account = Stripe::Account.retrieve(current_user.merchant_id)
+    #     account.payout_schedule.delays_days = 7
+    #     account.payout_schedule.interval    = 'daily'
         
-        account.save
-        logger.debug "#{account}"
-      end
+    #     account.save
+    #     logger.debug "#{account}"
+    #   end
       
-      sign_in_and_redirect @user, event: :authentication
-      flash[:success] = "Stripe account created and connected" if is_navigational_format?
+    #   sign_in_and_redirect @user, event: :authentication
+    #   flash[:success] = "Stripe account created and connected" if is_navigational_format?
       
-    else
+    # else
       
-      session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
+    #   session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       
-      redirect_to dashboard_path
-    end
+    #   redirect_to dashboard_path
+    # end
   end
 end
