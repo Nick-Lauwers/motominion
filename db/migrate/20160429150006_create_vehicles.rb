f473a56fdf3ddd8f99ec425a3dc33c75b5db89a8
yukon519 <nicholaslauwers04@gmail.com>
2018-03-28 14:23:52 +0000

    Initialize motominion repository
    Former-commit-id: a3f42fc255555587d96154c30c8e0600f30db3bd

--- db/migrate/20160429150006_create_vehicles.rb
+++ db/migrate/20160429150006_create_vehicles.rb
@@ -2,30 +2,31 @@ class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
       
-      t.string      :body_style
-      t.string      :color
-      t.string      :transmission
-      t.string      :fuel_type
-      t.string      :drivetrain
-      t.string      :vin
-      t.string      :listing_name
-      t.string      :street_address
-      t.string      :apartment
-      t.string      :city
-      t.string      :state
-      t.integer     :year
-      t.integer     :price
-      t.integer     :mileage
-      t.integer     :seating_capacity
-      t.text        :summary
-      t.text        :sellers_notes
-      t.boolean     :is_leather_seats
-      t.boolean     :is_sunroof 
-      t.boolean     :is_navigation_system 
-      t.boolean     :is_dvd_entertainment_system 
-      t.boolean     :is_bluetooth
-      t.boolean     :is_backup_camera
-      t.boolean     :is_remote_start
+      t.string      :body_style (cruiser, dual-sport, mini / pocket, moped, sportbike, standard, touring, trike, vintage)
+      t.string      :color (keep)
+      t.string      :transmission (engine-type: single, parallel-twin, inline-three, inline-four, v-twin, V4, flat-twin [boxer], L-twin, other, unknown)
+      t.string      :fuel_type (keep)
+      t.string      :vin (keep)
+      t.string      :listing_name (keep)
+      t.string      :street_address (keep)
+      t.string      :apartment (keep)
+      t.string      :city (keep)
+      t.string      :state (keep)
+      t.integer     :year (keep)
+      t.integer     :price (keep)
+      t.integer     :mileage (keep)
+      t.integer     :engine_size (keep)
+      t.text        :summary (keep)
+      t.text        :sellers_notes (keep)
+      t.boolean     :is_windscreen
+      
+      t.boolean     :is_leather_seats (is_windscreen)
+      t.boolean     :is_sunroof (is_heated_seats, is_heated_hand_grips)
+      t.boolean     :is_navigation_system (is_sidecar)
+      t.boolean     :is_dvd_entertainment_system (is_tow_hitch)
+      t.boolean     :is_bluetooth (is_trunk, is_saddlebags)
+      t.boolean     :is_backup_camera (is_navigation_system)
+      t.boolean     :is_remote_start (is_alarm_system, is_cb_radio, is_cruise_control, is_trailer, is_cycle_cover)
       t.boolean     :is_tow_package
       t.references  :user,          index: true, foreign_key: true
       t.references  :vehicle_make,  index: true, foreign_key: true
