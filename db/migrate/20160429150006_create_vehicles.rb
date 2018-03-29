f0c88f078c69ee3ea4513d00de3aaaa332545609
yukon519 <nicholaslauwers04@gmail.com>
2018-03-29 19:01:31 +0000

    Repair migrations and seed data
    Former-commit-id: a1ba2312a473c1499b78b39072906eb65866d8de

--- db/migrate/20160429150006_create_vehicles.rb
+++ db/migrate/20160429150006_create_vehicles.rb
@@ -2,32 +2,33 @@ class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
       
-      t.string      :body_style (cruiser, dual-sport, mini / pocket, moped, sportbike, standard, touring, trike, vintage)
-      t.string      :color (keep)
-      t.string      :transmission (engine-type: single, parallel-twin, inline-three, inline-four, v-twin, V4, flat-twin [boxer], L-twin, other, unknown)
-      t.string      :fuel_type (keep)
-      t.string      :vin (keep)
-      t.string      :listing_name (keep)
-      t.string      :street_address (keep)
-      t.string      :apartment (keep)
-      t.string      :city (keep)
-      t.string      :state (keep)
-      t.integer     :year (keep)
-      t.integer     :price (keep)
-      t.integer     :mileage (keep)
-      t.integer     :engine_size (keep)
-      t.text        :summary (keep)
-      t.text        :sellers_notes (keep)
-      t.boolean     :is_windscreen
-      
-      t.boolean     :is_leather_seats (is_windscreen)
-      t.boolean     :is_sunroof (is_heated_seats, is_heated_hand_grips)
-      t.boolean     :is_navigation_system (is_sidecar)
-      t.boolean     :is_dvd_entertainment_system (is_tow_hitch)
-      t.boolean     :is_bluetooth (is_trunk, is_saddlebags)
-      t.boolean     :is_backup_camera (is_navigation_system)
-      t.boolean     :is_remote_start (is_alarm_system, is_cb_radio, is_cruise_control, is_trailer, is_cycle_cover)
-      t.boolean     :is_tow_package
+      t.string      :body_style
+      t.string      :color
+      t.string      :engine_type
+      t.string      :fuel_type
+      t.string      :vin
+      t.string      :listing_name
+      t.string      :street_address
+      t.string      :apartment
+      t.string      :city
+      t.string      :state
+      t.integer     :year
+      t.integer     :price
+      t.integer     :mileage
+      t.integer     :engine_size
+      t.text        :summary
+      t.text        :sellers_notes
+      t.boolean     :is_cruise_control
+      t.boolean     :is_am_fm
+      t.boolean     :is_cb_radio
+      t.boolean     :is_navigation_system
+      t.boolean     :is_heated_seats
+      t.boolean     :is_heated_hand_grips
+      t.boolean     :is_alarm_system
+      t.boolean     :is_saddlebags
+      t.boolean     :is_trunk
+      t.boolean     :is_tow_hitch
+      t.boolean     :is_cycle_cover
       t.references  :user,          index: true, foreign_key: true
       t.references  :vehicle_make,  index: true, foreign_key: true
       t.references  :vehicle_model, index: true, foreign_key: true
