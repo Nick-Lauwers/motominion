c5a4b99c3e8dac27f5faaacbb500878c2a9af195
yukon519 <nicholaslauwers04@gmail.com>
2018-03-30 18:07:58 +0000

    Configure DNS settings and begin css modifications
    Former-commit-id: 7605b10896f0b06ea46cdbb35e0ffe33e846e86a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     
-    base_title = "Mototrova"
+    base_title = "Motominion"
     
     if page_title.empty?
       base_title
