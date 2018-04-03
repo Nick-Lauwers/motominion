29a1c442ec66d615d96e2777d24337b9511e2676
yukon519 <nicholaslauwers04@gmail.com>
2018-04-03 03:03:54 +0000

    Correct omniauth errors
    Former-commit-id: 71b143729124eb40ecde15ff4aa1fa89a7af93a8

--- app/models/business_hour.rb
+++ app/models/business_hour.rb
@@ -2,5 +2,5 @@ class BusinessHour < ActiveRecord::Base
   
   belongs_to :dealership
   
-  validates :day, :open_time, :close_time, presence: true
+  # validates :day, :open_time, :close_time, presence: true
 end
