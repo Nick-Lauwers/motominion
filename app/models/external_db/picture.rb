f388f2837acd7c7966614432027ad0ba5acebafc
yukon519 <nicholaslauwers04@gmail.com>
2018-07-27 01:59:34 +0000

    Correct synchronization (5)
    Former-commit-id: 04ca67a929dafe155b39149cb123c2342752e10d

--- app/models/external_db/picture.rb
+++ app/models/external_db/picture.rb
@@ -14,24 +14,25 @@ module ExternalDb
           end
 
         else
+          
           ::Photo.new(scraped_id: id).tap do |p|
-            
             p.vehicle = ::Vehicle.where(scraped_id: vehicle_id).first
             p.image = open(image_url.gsub! '300x225', '800x600') rescue nil
             p.last_found_at = last_found
             p.save or nil
+          end
+          
+          ::Vehicle.where(scraped_id: vehicle_id).first do |v|
+              
+            if v.photos.count.between?(1,3)
+              v.listing_score.update_attribute(:photos_score, 33)
+            elsif v.photos.count.between?(4,7)
+              v.listing_score.update_attribute(:photos_score, 67)
+            elsif v.photos.count >= 8
+              v.listing_score.update_attribute(:photos_score, 100)
+            end
             
-            ::Vehicle.where(scraped_id: vehicle_id).first do |v|
-              
-              if v.photos.count.between?(1,3)
-                v.listing_score.update_attribute(:photos_score, 33)
-              elsif p.vehicle.photos.count.between?(4,7)
-                v.listing_score.update_attribute(:photos_score, 67)
-              else
-                v.listing_score.update_attribute(:photos_score, 100)
-              end
-              
-              v.listing_score.update_attribute(overall_score: 
+            v.listing_score.update_attribute(:overall_score, 
                                   ( v.listing_score.location_score + 
                                     v.listing_score.features_score + 
                                     v.listing_score.spec_score + 
@@ -43,7 +44,6 @@ module ExternalDb
                                     # v.listing_score.reviews_score + 
                                     v.listing_score.recently_posted_score + 
                                     v.listing_score.many_listings_score ) / 12 )
-            end
           end
         end 
       end
