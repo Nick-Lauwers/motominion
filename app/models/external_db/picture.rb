dc4fd01d7b081fb8007495e28b488bea3168680c
yukon519 <nicholaslauwers04@gmail.com>
2018-07-27 00:11:56 +0000

    Repair synchronization (3)
    Former-commit-id: da381fc6002fe93aa83ff631aae657f9ca53e766

--- app/models/external_db/picture.rb
+++ app/models/external_db/picture.rb
@@ -19,6 +19,30 @@ module ExternalDb
             p.image = open(image_url.gsub! '300x225', '800x600') rescue nil
             p.last_found_at = last_found
             p.save or nil
+            
+            p.vehicle do |v|
+              
+              if v.photos.count.between?(1,3)
+                v.listing_score.update_attribute(photos_score: 33)
+              elsif p.vehicle.photos.count.between?(4,7)
+                v.listing_score.update_attribute(photos_score: 67)
+              else
+                v.listing_score.update_attribute(photos_score: 100)
+              end
+              
+              v.listing_score.update_attribute(overall_score: 
+                                  ( v.listing_score.location_score + 
+                                    v.listing_score.features_score + 
+                                    v.listing_score.spec_score + 
+                                    v.listing_score.vin_score + 
+                                    v.listing_score.certified_dealer_score +
+                                    v.listing_score.direct_listing_score +
+                                    v.listing_score.test_drive_score + 
+                                    ( 3 * v.listing_score.photos_score ) +
+                                    # v.listing_score.reviews_score + 
+                                    v.listing_score.recently_posted_score + 
+                                    v.listing_score.many_listings_score ) / 12 )
+            end
           end
         end 
       end
