7e303f1b7eba0f2520f0f5b701c3457994e87495
yukon519 <nicholaslauwers04@gmail.com>
2018-07-27 02:58:51 +0000

    Correct synchronization (7)
    Former-commit-id: 69f066377ff0eb62345589fcf33274d6121ac749

--- app/models/external_db/picture.rb
+++ app/models/external_db/picture.rb
@@ -14,36 +14,11 @@ module ExternalDb
           end
 
         else
-          
           ::Photo.new(scraped_id: id).tap do |p|
             p.vehicle = ::Vehicle.where(scraped_id: vehicle_id).first
             p.image = open(image_url.gsub! '300x225', '800x600') rescue nil
             p.last_found_at = last_found
             p.save or nil
-          end
-          
-          ::Photo.where(scraped_id: id).first.vehicle do |v|
-              
-            if v.photos.count.between?(1,3)
-              v.listing_score.update_attribute(:photos_score, 33)
-            elsif v.photos.count.between?(4,7)
-              v.listing_score.update_attribute(:photos_score, 67)
-            elsif v.photos.count >= 8
-              v.listing_score.update_attribute(:photos_score, 100)
-            end
-            
-            v.listing_score.update_attribute(:overall_score, 
-                                  ( v.listing_score.location_score + 
-                                    v.listing_score.features_score + 
-                                    v.listing_score.spec_score + 
-                                    v.listing_score.vin_score + 
-                                    v.listing_score.certified_dealer_score +
-                                    v.listing_score.direct_listing_score +
-                                    v.listing_score.test_drive_score + 
-                                    ( 3 * v.listing_score.photos_score ) +
-                                    # v.listing_score.reviews_score + 
-                                    v.listing_score.recently_posted_score + 
-                                    v.listing_score.many_listings_score ) / 12 )
           end
         end 
       end
--- lib/tasks/synchronize.rake
+++ lib/tasks/synchronize.rake
@@ -1,5 +1,30 @@
 task :synchronize => :environment do
+  
   ExternalDb::Source.all.each(&:sync_to_dealership)
   ExternalDb::Vehicle.all.each(&:sync_to_vehicle)
   ExternalDb::Picture.all.each(&:sync_to_photo)
+  
+  Vehicle.all.each do |v|
+              
+    if v.photos.count.between?(1,3)
+      v.listing_score.update_attribute(photos_score: 33)
+    elsif v.photos.count.between?(4,7)
+      v.listing_score.update_attribute(photos_score: 67)
+    elsif v.photos.count >= 8
+      v.listing_score.update_attribute(photos_score: 100)
+    end
+    
+    v.listing_score.update_attribute(overall_score: 
+                                  ( v.listing_score.location_score + 
+                                    v.listing_score.features_score + 
+                                    v.listing_score.spec_score + 
+                                    v.listing_score.vin_score + 
+                                    v.listing_score.certified_dealer_score +
+                                    v.listing_score.direct_listing_score +
+                                    v.listing_score.test_drive_score + 
+                                    ( 3 * v.listing_score.photos_score ) +
+                                    # v.listing_score.reviews_score + 
+                                    v.listing_score.recently_posted_score + 
+                                    v.listing_score.many_listings_score ) / 12 )
+  end
 end
