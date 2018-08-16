a266bb1a7219fb2c9a882c3f7024772db84dce20
yukon519 <nicholaslauwers04@gmail.com>
2018-08-16 23:05:27 +0000

    Correct dealer select
    Former-commit-id: ddae0ce53fcec96b854af581818a6bd174603e24

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,12 +1,20 @@
 $(function() {
 
   // form
-
+    
     // smooth scroll
     
-      var availabilityLink = $('.availability-link');
+      var availabilityLink         = $('.availability-link');
+      var testDriveSidenavInfoLink = $('.test-drive-sidenav-info a');
     
       availabilityLink.click(function(evn) {
+        evn.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#availability').offset().top-50
+        }, 'slow');
+      });
+      
+      testDriveSidenavInfoLink.click(function(evn) {
         evn.preventDefault();
         $('html, body').animate({
             scrollTop: $('#availability').offset().top-50
@@ -23,5 +31,29 @@ $(function() {
           minDate:           Date(),
           widgetPositioning: { horizontal: 'auto', vertical: 'bottom' }
         });
+      });
+    
+    // list scroll
+      
+      var testDriveSidenavLeft   = $('.test-drive-sidenav-left');
+      var testDriveSidenavRight  = $('.test-drive-sidenav-right');
+      var testDriveSidenavScroll = $('.test-drive-sidenav-scroll');
+
+      testDriveSidenavLeft.click(function() {
+        
+        event.preventDefault();
+        
+        testDriveSidenavScroll.animate({
+          scrollLeft: "-=150px"
+        }, "slow");
+      });
+      
+      testDriveSidenavRight.click(function() {
+        
+        event.preventDefault();
+        
+        testDriveSidenavScroll.animate({
+          scrollLeft: "+=150px"
+        }, "slow");
       });
 });
