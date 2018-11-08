8881a586ee3ed38302cba817f48cbbd0e00c2768
yukon519 <nicholaslauwers04@gmail.com>
2018-11-08 18:22:08 +0000

    Remove password protections for vehicle feed
    Former-commit-id: cde2ba55975360e786834bd335a8d61bf220d9ce

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -5,7 +5,10 @@ class VehiclesController < ApplicationController
   require 'will_paginate/array'
   
   before_action :logged_in_user,     except: [:show, :search, :autocomplete, 
-                                              :feed]
+                                              :feed_1, :feed_2, :feed_3, 
+                                              :feed_4, :feed_5, :feed_6, 
+                                              :feed_7, :feed_8, :feed_9, 
+                                              :feed_10]
   before_action :profile_pic_upload, only:   [:post]
   before_action :get_vehicle,        only:   [:destroy, :show, :update, :basics,
                                               :details, :upgrades, :photos, 
