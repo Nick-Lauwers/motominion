7ddca1f3d683841ddaee9bcdb41c87defd3c6d8b
yukon519 <nicholaslauwers04@gmail.com>
2018-12-27 18:51:11 +0000

    Adjust search order
    Former-commit-id: 708aa684c4889f9f2dd15d68650ca20c8199729e

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -263,9 +263,10 @@ class VehiclesController < ApplicationController
     @vehicles = @filterrific.
                   find.
                   includes(:listing_score).
-                  order("listing_scores.overall_score DESC",
-                        "year DESC",
-                        "listing_name ASC").
+                  order("listing_scores.overall_score DESC").
+                        # ,
+                        # "year DESC",
+                        # "listing_name ASC").
                   paginate(page: params[:page], per_page: 10)
 
     # @hash = Gmaps4rails.build_markers(@vehicles) do |vehicle, marker|
