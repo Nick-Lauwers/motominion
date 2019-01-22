c06e67e96301338d04b9314584b2e23722ce06af
yukon519 <nicholaslauwers04@gmail.com>
2019-01-22 02:00:17 +0000

    Rephrase login email
    Former-commit-id: 7188051b178b2a39b677dc84da88cbe3305a9902

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:failure] = "Please log in."
+        # flash[:failure] = "Please log in."
         redirect_to login_url
       end
     end
