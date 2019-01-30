8be8af07fca72b56d92b1665502dab7cbe9cc3fa
yukon519 <nicholaslauwers04@gmail.com>
2019-01-30 05:53:04 +0000

    Correct lead generation email bugs (3)
    Former-commit-id: a1dacb3cd91addda7529075ce78591901023858f

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -7,15 +7,28 @@ class MessageMailer < ApplicationMailer
     @sender       = message.user
     @conversation = message.conversation
     
-    mail to: @recipient.email, subject: "#{Emoji.find_by_alias("bell").raw} #{@sender.first_name} sent you a message!"
+    if @sender.dealership.present?
+      mail to: @recipient.email, 
+      subject: "#{Emoji.find_by_alias("bell").raw} #{@sender.dealership.dealership_name} sent you a message!"
+    
+    else
+      mail to: @recipient.email, 
+      subject: "#{Emoji.find_by_alias("bell").raw} #{@sender.first_name} sent you a message!"
+    end
   end
   
   def message_received_admin(message)
     
+    @message      = message.content
     @recipient    = message.user == message.conversation.sender ? message.conversation.recipient : message.conversation.sender
     @sender       = message.user
-    @conversation = message.conversation
     
-    mail to: "nlauwers@motominion.com", subject: "#{Emoji.find_by_alias("bell").raw} #{@recipient.first_name} sent #{@sender.first_name} a message!"
+    if @sender.dealership.present?
+      mail to: "nlauwers@motominion.com", 
+      subject: "#{Emoji.find_by_alias("bell").raw} #{@sender.dealership.dealership_name} sent a message!"
+    else 
+      mail to: "nlauwers@motominion.com", 
+      subject: "#{Emoji.find_by_alias("bell").raw} #{@sender.first_name} sent a message!"
+    end
   end
 end
